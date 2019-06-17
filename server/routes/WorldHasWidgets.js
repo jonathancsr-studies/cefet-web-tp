15b89a4f3353be3b549f46181496a8d3cc07b7ba
Your Name <you@example.com>
2019-06-17 11:32:46 -0300

    Readme.md do server adicionado e Rota WorldHasWidg

--- server/routes/WorldHasWidgets.js
+++ server/routes/WorldHasWidgets.js
@@ -7,6 +7,30 @@ const bcrypt = require('bcrypt')
 const WorldHasWidgets = require("../models/worldHasWidgets")
 worldHasWidgets.use(cors())
 
+worldHasWidgets.post('/registerWidgets', (req,res) =>{
+    const worldHasWidgetsData ={
+        id_world : req.body.id_world,
+        id_widgets : req.body.id_widgets 
+    }
+    WorldHasWidgets.findOne({
+        where: {
+            id_world : req.body.id_world,
+            id_widgets : req.body.id_widgets
+        }
+    }).then(hasWidget =>{
+        if(!hasWidget){
+            WorldHasWidgets.create(worldHasWidgetsData)
+                .then(hasWidget => {
+                    res.json({status : hasWidget})
+                })
+                .catch(err =>{
+                    res.send('error: ' + err)
+                })
+        }
+    })
+
+})
+
 worldHasWidgets.get('/findById', (req, res) => {
     WorldHasWidgets.findOne({
         where: {
