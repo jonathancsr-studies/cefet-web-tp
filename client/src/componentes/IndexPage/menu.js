c60ed8167a8ebad6b422e8bda7fcf8dfcaede914
jonathancsr <jonathan.csr@hotmail.com>
2019-07-01 22:34:06 -0300

    jonathan alteraçoes

--- client/src/componentes/IndexPage/menu.js
+++ client/src/componentes/IndexPage/menu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
 import { Link, withRouter } from 'react-router-dom'
+import ColorPicker from '../World/preferences/ColorPicker'
 
 import './loginAndRegister.css'
 import Register from './Register'
@@ -41,6 +42,7 @@ class menu extends Component {
                     <Navbar fixed="top" className="justify-content-between" bg="dark" variant="dark">
                         <Navbar.Brand><Link className='textGoldColor' onClick={this.loginLogo}><img className='logo' src={logo} /></Link></Navbar.Brand>
                         <Navbar.Brand >
+                            <ColorPicker /> 
                             <Button className='textGoldColor' variant="primary blueBackgroundColor marginCenter textGoldColor" type="submit">
                                 <Link to='/' onClick={this.logOut.bind(this)}>
                                     Logout </Link>
