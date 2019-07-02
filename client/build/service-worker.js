06790219167b3bf38c9bd08492f73fe59dddc2e2
jonathancsr <jonathan@logpyx.com>
2019-07-02 09:21:36 -0300

    asd

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
-  "/myuniverselol/precache-manifest.af132a1a80bee6948e9ad254c9a00b05.js"
-=======
   "/cefet-web-tp/precache-manifest.bdd65b3e00eb436d03118248691e4297.js"
->>>>>>> ad402a2b35425999215ea5e7aa695ada21273b4a
 );
 
 self.addEventListener('message', (event) => {
